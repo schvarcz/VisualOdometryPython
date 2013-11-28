2a869daf5b3d366b690f6732267ce7bc6350d211
Guilherme Schvarcz Franco <guilhermefrancosi@gmail.com>
2013-11-28 15:05:37 -0200

    Adiociona plotagem do VisualOdometry do LibVisio/GPS do dataset/Nossos resultados

--- plot.py
+++ plot.py
@@ -5,82 +5,104 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
+def matrixRot(alpha,beta,gama):
+    return asarray(
+    [[cos(alpha)*cos(gama)-sin(alpha)*cos(beta)*sin(gama) ,  sin(alpha)*cos(gama)+cos(alpha)*cos(beta)*sin(gama) ,  sin(beta)*sin(gama)],
+     [-cos(alpha)*sin(gama)-sin(alpha)*cos(beta)*cos(gama),  -sin(alpha)*sin(gama)+cos(alpha)*cos(beta)*cos(gama),  sin(beta)*cos(gama) ],
+     [sin(beta)*sin(gama)                                 ,  -cos(alpha)*sin(beta)                               ,  cos(beta)]])
+
+
+
 f = plt.figure()
-f1 = plt.figure()
 ax = f.add_subplot(111,projection='3d')
 ax.set_xlim3d(-100,100)
 ax.set_ylim3d(-100,100)
 
 trans = [[],[],[]]
 rots = [[],[],[]]
-oldT = zeros(3)
-#oldT[3] = 1.
-#oldT = matrix(oldT).T
+oldT = asarray([.6,.05,1.6])
+oldR = asarray([0.,-.08,0.])
 
-oldR = ones(3)
 for line in csv.reader(file("posicao.csv"),delimiter=';'):
     line = [float(l) for l in line]
+
     t = asarray(line[0:3])
-
     r = asarray(line[3:])
-    alpha, beta, gama = r
-#    m = column_stack((r,t))
-#    m = row_stack((m,zeros(4)))
-#    m[3][3]=1.
-#    m = matrix(m)
-    
-    #oldT= m.I*oldT
-    
-    #t = oldR.dot(t)
-    #oldR = oldR.dot(r)
-    t += oldT
-    oldT = t
-    oldR += r
 
+    alpha, beta, gama = r
+
+    alpha, beta, gama = oldR
+
+    x, y, z = list(oldT)
+    #t = asarray([y,z,x])
+
+    t = matrixRot(-alpha,-beta,-gama).dot(t)
+
+    oldR += r
+    oldT += t
 
     x, y, z = list(oldT)
 
-    if(abs(alpha) > 15):
-        continue
     trans[0].append(x)
-    trans[1].append(y)
-    trans[2].append(z)
+    trans[1].append(-y)
+    trans[2].append(-z)
 
     rots[0].append(alpha)
     rots[1].append(beta)
     rots[2].append(gama)
-#    for i in range(3):
-#        trans[i].append(t[i])
+
     print oldT
 
 
 print oldT, oldR
-#ax.plot(trans[0],trans[2],trans[1])
-print min(rots[0])
+#ax.plot(trans[1],trans[2],zeros(len(trans[0])))
+ax.plot(trans[1],trans[2],trans[0])
+#ax.plot(trans[0],trans[1],trans[2])
+#ax.plot(trans[0],trans[2],zeros(len(trans[1])))
+
+
+
+
+transReal = [[],[],[]]
+tInicial = None
+for line in csv.reader(file("/home/schvarcz/Dropbox/UFRGS - Mestrado/Robótica Avançada/Visual Odometry/disparity_stereo/dataset_libviso/insdata.txt"),delimiter=' '):
+    t = asarray([float(l) for l in line[4:7]])
+    t = matrixRot(-0.78539816339744828,0,0).dot(t)
+    if tInicial == None:
+        tInicial = t
+    t = t-tInicial
+    for i in range(3):
+        transReal[i].append(t[i])
+    
+ax.plot(transReal[0],transReal[1],zeros(len(transReal[2])))
+
+ax.set_xlim3d(-100,100)
+ax.set_ylim3d(-100,100)
+ax.set_zlim3d(-100,100)
+
+
+
+
+transLibViso = [[],[],[]]
+for line in csv.reader(file("saida.txt"),delimiter=';'):
+    x,y,z = [float(l.split(' ')[-2]) for l in line][:3]
+    
+    transLibViso[0].append(x)
+    transLibViso[1].append(y)
+    transLibViso[2].append(z)
+
+
+ax.plot(transLibViso[0],transLibViso[2],transLibViso[1])
+
+
+
+f1 = plt.figure()
+plt.plot(trans[1],trans[2])
+plt.plot(transReal[0],transReal[1])
+plt.plot(transLibViso[0],transLibViso[2])
 #plt.plot(rots[0])
-plt.plot(rots[1])
+#plt.plot(rots[1])
 #plt.plot(rots[2])
 plt.show()
 
 
-
-
-
-#transReal = [[],[],[]]
-#tInicial = None
-#for line in csv.reader(file("/home/schvarcz/Dropbox/UFRGS - Mestrado/Robótica Avançada/Visual Odometry/disparity_stereo/dataset_libviso/insdata.txt"),delimiter=' '):
-#    t = asarray([float(l) for l in line[4:7]])
-#    print t
-#    if tInicial == None:
-#        tInicial = t
-#    t = t-tInicial
-#    for i in range(3):
-#        transReal[i].append(t[i])
-#    
-#ax.plot(transReal[0],transReal[1],transReal[2])
-
-ax.set_xlim3d(0,100)
-ax.set_ylim3d(0,100)
-ax.set_zlim3d(0,200)
-f.show()
-raw_input()
